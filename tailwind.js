8303d01ba6d7116a3e28f91dd5fbeca69d31a18e
Marina <marinanogueira934@gmail.com>
2023-09-25 05:10:48 -0300

    Configurações iniciais

---
+++ tailwind.js
@@ -0,0 +1,28 @@
+tailwind.config = {
+    content: ["../../app/**/*.{php,html,js}"],
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+    theme: {
+      extend: {
+        colors: {
+          background: "#4277FF",
+          title: "#0099BA",
+          lightred: "#FF4B4B",
+          lightorange: "#FFA14A",
+          darkblue: "#16009E",
+          purple: "#5200FF", 
+          pink: "#FF008A",
+          limegreen: "#85FF91",
+          orange: "#FF9900",
+          white: "#fff",
+          black: "#000"
+        },
+      },
+      fontFamily: {
+        font: ["Mukta", "sans-serif"],
+      },
+    },
+  };
+  
